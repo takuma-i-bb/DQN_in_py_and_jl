49803ad1ea176c8178b14574d0cc62e8d4fa4898
ikeda <ikeda@fz.iit.tsukuba.ac.jp>
2022-05-04 00:22:07 +0900

    Struggling with Flux

--- DQN.jl
+++ DQN.jl
@@ -1,11 +1,13 @@
 using StatsBase: StatsBase
 using CUDA
+using PyCall
+using Flux
 
 struct Transition
-    s::Tuple
+    s::Vector
     a::Int
     r::Int
-    s_prime::Tuple
+    s_prime::Vector
     done::Int
 end
 
@@ -26,19 +28,47 @@ function sample(replaybuffer::ReplayBuffer, n::Int)
     for detum in mini_batch
         push!(s_list, detum.s)
         push!(a_list, )
+    end
 end
 
 function Base.length(replaybuffer::ReplayBuffer)
     length(replaybuffer.buffer)
 end
 
-Q_function = Chain(
-    Dense(4 => 128, relu),
-    Dense(128 => 128, relu),
-    Dense(128 => 2)
-) |> gpu
+struct DQN
+    env::PyObject
+    Q_function
+end
 
-loss(x, y) = Flux.Losses.huber_loss(Q_function(x), y)
+function DQN(env::PyObject)
+    Q_function = Chain(
+        Dense(4 => 128, relu),
+        Dense(128 => 128, relu),
+        Dense(128 => 2)
+    )
+    return DQN(env, Q_function)
+end
 
-a = ReplayBuffer()
-println(Q_function(rand(4)))
+function select_action(dqn::DQN, s::Vector)
+    qs = q_forward(dqn, s)
+    return argmax(qs)
+end
+
+q_forward(dqn::DQN, s::Vector) = dqn.Q_function(s)
+loss(x, y) = Flux.Losses.huber_loss(x, y)
+
+function dqn_train!(dqn::DQN, dqn_target::DQN, replaybuffer::ReplayBuffer, optim::Flux.Optimiser)
+    data = sample(replaybuffer, batch_size)
+end
+
+#main
+const α = 0.0005
+const γ = 0.98
+const buffer_limit = 50000
+const batch_size = 32
+
+const Gym = pyimport("gym")
+buf = ReplayBuffer()
+env = Gym.make("CartPole-v1")
+dqn = DQN(env)
+dqn_target = deepcopy(dqn)
